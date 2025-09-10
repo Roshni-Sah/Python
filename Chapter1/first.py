9467b07d97e3eecd2aa31d697dbd3b65d37d467f
Roshni Sah <155195205+Roshni-Sah@users.noreply.github.com>
2025-09-10 10:59:53 +0530

    Add Chapter 1

---
+++ Chapter1/first.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+
+import pyjokes
+
+# print("Printing Jokes...")
+
+# This prints a random joke
+joke = pyjokes.get_joke()
+print(joke)
---
+++ Chapter1/problem2.py
@@ -0,0 +1,6 @@
+#Install an external module and use it to preform an operation.
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hey I am good")
+engine.runAndWait()
